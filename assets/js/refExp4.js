6a72d689f24a4d1b45e031afc101d7f519dcb4ce
Sergey <temp.sergey4@gmail.com>
2026-03-24 19:23:52 +0200

    24.03.2026

---
+++ assets/js/refExp4.js
@@ -0,0 +1,43 @@
+const form = document.querySelector('form');
+
+// form.addEventListener('submit', submitHandler);
+
+// function submitHandler (e) {    
+//     e.preventDefault();
+
+
+// const [nameInput] = document.querySelectorAll('input');
+
+//    if (/^[A-Z][a-z]{2,19}$/.test(nameInput.value)) {
+//     nameInput.classList.add('valid');
+//     nameInput.classList.remove('invalid');
+// }
+//     else {
+//     nameInput.classList.remove('valid');
+//     nameInput.classList.add('invalid');
+// }
+// }
+
+
+///
+
+
+// const [nameInput] = document.querySelectorAll('input');nameInput.addEventListener('input', inputNameHandler);
+
+// function inputNameHandler (e) {    
+//     if (/^[A-Z][a-z]{1,19}$/.test(e.target.value)) {
+//     nameInput.classList.add('valid');
+//     nameInput.classList.remove('invalid');
+// }
+//     else {
+//     nameInput.classList.remove('valid');
+//     nameInput.classList.add('invalid');
+// }
+// }
+
+////////////////////////////////////////////////////
+
+
+
+
+
